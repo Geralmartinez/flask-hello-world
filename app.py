9acd016c614784ba4e5c191e5a68b2d1716f759c
Geralmartinez <127754703+Geralmartinez@users.noreply.github.com>
2023-04-18 14:12:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+@app.route('/foto')
+def hello_foto_world():
+    return render_template('/foto.html')
